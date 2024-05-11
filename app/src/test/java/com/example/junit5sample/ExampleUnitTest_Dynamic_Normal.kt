97254583d6622e8933fdcec9b2e41ffc6035ddf8
kiyoshisaito <kiyoshi.saito@tttsunagari.jp>
2024-05-11 21:21:56 +0900

    リファクタリング

--- app/src/test/java/com/example/junit5sample/ExampleUnitTest_Dynamic_Normal.kt
+++ app/src/test/java/com/example/junit5sample/ExampleUnitTest_Dynamic_Normal.kt
@@ -37,7 +37,7 @@ class ExampleUnitTest_Dynamic_Normal {
     private fun t() {
         actual = actor.fizzBuzz(arg)
     }
-    @TF fun `引数が 3 の倍数の場合`(): List<DynamicNode> {
+    @TF@N("引数が 3 の倍数の場合") fun `can divide 3`(): List<DynamicNode> {
         arg = 3
         t()
         return listOf(
@@ -48,7 +48,7 @@ class ExampleUnitTest_Dynamic_Normal {
             "リポジトリの取得2がされない" { verify(exactly = 0) { repository.fetch2() } },
         )
     }
-    @TF fun `引数が 5 の倍数の場合`(): List<DynamicNode> {
+    @TF@N("引数が 5 の倍数の場合") fun `can divide 5`(): List<DynamicNode> {
         arg = 5
         t()
         return listOf(
@@ -59,7 +59,7 @@ class ExampleUnitTest_Dynamic_Normal {
             "リポジトリの取得2がされない" { verify(exactly = 0) { repository.fetch2() } },
         )
     }
-    @TF fun `引数が 3 と 5 の倍数の場合`(): List<DynamicNode> {
+    @TF@N("引数が 3 と 5 の倍数の場合") fun `can divide 3, 5 `(): List<DynamicNode> {
         arg = 15
         t()
         return listOf(
@@ -71,12 +71,11 @@ class ExampleUnitTest_Dynamic_Normal {
         )
     }
     @Ne @N("引数が 3の倍数でも 5の倍数でもない場合")
-    inner class `can not divide 3 or 5` {
-        @BeforeEach
-        fun setup() {
+    inner class `can not divide 3, 5` {
+        @BeforeEach fun setup() {
             arg = 4
         }
-        @TF @N("共通仕様") fun common(): List<DynamicNode> {
+        @TF@N("共通仕様") fun `can not divide 3, 5 common`(): List<DynamicNode> {
             t()
             return listOf(
                 "fizz が非表示" { assertFalse(actual.fizz) },
@@ -84,7 +83,7 @@ class ExampleUnitTest_Dynamic_Normal {
                 "リポジトリの取得1がされる" { verify(exactly = 1) { repository.fetch1() } },
             )
         }
-        @TF @N("取得1のタイプが A の場合") fun `fetch1 type is A`(): List<DynamicNode> {
+        @TF@N("取得1のタイプが A の場合") fun `fetch1 type is A`(): List<DynamicNode> {
             every { repository.fetch1() } returns Result("A")
             t()
             return listOf(
@@ -92,28 +91,31 @@ class ExampleUnitTest_Dynamic_Normal {
                 "リポジトリの取得2がされない" { verify(exactly = 0) { repository.fetch2() } },
             )
         }
-        @Ne @N("取得1のタイプが B の場合") inner class `fetch1 type is B` {
-            @BeforeEach
-            fun setup() {
+        @Ne@N("取得1のタイプが B の場合") inner class `fetch1 type is B` {
+            @BeforeEach fun setup() {
                 every { repository.fetch1() } returns Result("B")
             }
-            @TF fun `取得2のタイプが X の場合`(): DynamicNode {
+            @T@N("取得2が実行される") fun `fetch2 is executed`() {
+                t()
+                verify(exactly = 1) { repository.fetch2() }
+            }
+            @TF@N("取得2のタイプが X の場合") fun `fetch2 type is X`(): DynamicNode {
                 every { repository.fetch2() } returns Result2("X")
                 t()
                 return "その他 が XX で表示" { assertEquals("XX", actual.elseValue) }
             }
-            @TF fun `取得2のタイプが Y の場合`(): DynamicNode {
+            @TF@N("取得2のタイプが Y の場合") fun `fetch2 type is Y`(): DynamicNode {
                 every { repository.fetch2() } returns Result2("Y")
                 t()
                 return "その他 が YY で表示" { assertEquals("YY", actual.elseValue) }
             }
-            @TF fun `取得2のタイプが A でも B でもない場合`(): DynamicNode {
-                every { repository.fetch2() } returns Result2("C")
+            @TF@N("取得2のタイプが A でも B でもない場合") fun `fetch2 type is not X, Y`(): DynamicNode {
+                every { repository.fetch2() } returns Result2("Z")
                 t()
                 return "その他 が BB で表示" { assertEquals("BB", actual.elseValue) }
             }
         }
-        @TF fun `取得1のタイプが A でも B でもない場合`(): List<DynamicNode> {
+        @TF@N("取得1のタイプが A でも B でもない場合") fun `fetch type is not A, B`(): List<DynamicNode> {
             every { repository.fetch1() } returns Result("C")
             t()
             return listOf(
--- app/src/test/java/com/example/junit5sample/ExampleUnitTest_Dynamic_Spec.kt
+++ app/src/test/java/com/example/junit5sample/ExampleUnitTest_Dynamic_Spec.kt
@@ -1,11 +1,9 @@
-@file:Suppress("NonAsciiCharacters")
+@file:Suppress("NonAsciiCharacters", "TestFunctionName")
 
 package com.example.junit5sample
 
 import io.mockk.every
 import io.mockk.mockk
-import io.mockk.verify
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.DynamicNode
@@ -35,88 +33,90 @@ class ExampleUnitTest_Dynamic_Spec {
     private fun t() {
         actual = actor.fizzBuzz(arg)
     }
-    @TF fun `引数が 3 の倍数の場合`(): List<DynamicNode> {
+    @TF@N("引数が 3 の倍数の場合") fun `can divide 3`(): List<DynamicNode> {
         arg = 3
         t()
         return listOf(
-            "fizz が表示" { Assertions.assertTrue(actual.fizz) },
-            "buzz が非表示" { Assertions.assertFalse(actual.buzz) },
-            "その他 が非表示" { Assertions.assertEquals("", actual.elseValue) },
-            "リポジトリの取得1がされない" { verify(exactly = 0) { repository.fetch1() } },
-            "リポジトリの取得2がされない" { verify(exactly = 0) { repository.fetch2() } },
+            "fizz が表示" { actual.fizz shouldBe true },
+            "buzz が非表示" { actual.buzz shouldBe false },
+            "その他 が非表示" { actual.elseValue shouldBe "" },
+            "リポジトリの取得1がされない" { notExecute { repository.fetch1() } },
+            "リポジトリの取得2がされない" { notExecute { repository.fetch2() } },
         )
     }
-    @TF fun `引数が 5 の倍数の場合`(): List<DynamicNode> {
+    @TF@N("引数が 5 の倍数の場合") fun `can divide 5`(): List<DynamicNode> {
         arg = 5
         t()
         return listOf(
-            "fizz が非表示" { Assertions.assertFalse(actual.fizz) },
-            "buzz が表示" { Assertions.assertTrue(actual.buzz) },
-            "その他 が非表示" { Assertions.assertEquals("", actual.elseValue) },
-            "リポジトリの取得1がされない" { verify(exactly = 0) { repository.fetch1() } },
-            "リポジトリの取得2がされない" { verify(exactly = 0) { repository.fetch2() } },
+            "fizz が非表示" { actual.fizz shouldBe false },
+            "buzz が表示" { actual.buzz shouldBe true },
+            "その他 が非表示" { actual.elseValue shouldBe "" },
+            "リポジトリの取得1がされない" { notExecute { repository.fetch1() } },
+            "リポジトリの取得2がされない" { notExecute { repository.fetch2() } },
         )
     }
-    @TF fun `引数が 3 と 5 の倍数の場合`(): List<DynamicNode> {
+    @TF@N("引数が 3 と 5 の倍数の場合") fun `can divide 3, 5 `(): List<DynamicNode> {
         arg = 15
         t()
         return listOf(
-            "fizz が表示" { Assertions.assertTrue(actual.fizz) },
-            "buzz が表示" { Assertions.assertTrue(actual.buzz) },
-            "その他 が非表示" { Assertions.assertEquals("", actual.elseValue) },
-            "リポジトリ1の取得1がされない" { verify(exactly = 0) { repository.fetch1() } },
-            "リポジトリの取得2がされない" { verify(exactly = 0) { repository.fetch2() } },
+            "fizz が表示" { actual.fizz shouldBe true },
+            "buzz が表示" { actual.buzz shouldBe true },
+            "その他 が非表示" { actual.elseValue shouldBe "" },
+            "リポジトリ1の取得1がされない" { notExecute { repository.fetch1() } },
+            "リポジトリの取得2がされない" { notExecute { repository.fetch2() } },
         )
     }
     @Ne @N("引数が 3の倍数でも 5の倍数でもない場合")
-    inner class `can not divide 3 or 5` {
-        @BeforeEach
-        fun setup() {
+    inner class `can not divide 3, 5` {
+        @BeforeEach fun setup() {
             arg = 4
         }
-        @TF fun 共通仕様(): List<DynamicNode> {
+        @TF@N("共通仕様") fun `can not divide 3, 5 common`(): List<DynamicNode> {
             t()
             return listOf(
-                "fizz が非表示" { Assertions.assertFalse(actual.fizz) },
-                "buzz が非表示" { Assertions.assertFalse(actual.buzz) },
-                "リポジトリの取得1がされる" { verify(exactly = 1) { repository.fetch1() } },
+                "fizz が非表示" { actual.fizz shouldBe false },
+                "buzz が非表示" { actual.buzz shouldBe false },
+                "リポジトリの取得1がされる" { execute { repository.fetch1() } },
             )
         }
-        @TF fun `取得1のタイプが A の場合`(): List<DynamicNode> {
+        @TF@N("取得1のタイプが A の場合") fun `fetch1 type is A`(): List<DynamicNode> {
             every { repository.fetch1() } returns Result("A")
             t()
             return listOf(
-                "その他 が AA で表示" { Assertions.assertEquals("AA", actual.elseValue) },
-                "リポジトリの取得2がされない" { verify(exactly = 0) { repository.fetch2() } },
+                "その他 が AA で表示" { actual.elseValue shouldBe "AA" },
+                "リポジトリの取得2がされない" { notExecute { repository.fetch2() } },
             )
         }
-        @Ne @N("取得1のタイプが B の場合") inner class `fetch1 type is B` {
-            @BeforeEach
-            fun setup() {
+        @Ne@N("取得1のタイプが B の場合") inner class `fetch1 type is B` {
+            @BeforeEach fun setup() {
                 every { repository.fetch1() } returns Result("B")
             }
-            @TF fun `取得2のタイプが X の場合`(): DynamicNode {
+            @T@N("取得2が実行される") fun `fetch2 is executed`() {
+                t()
+                execute { repository.fetch2() }
+            }
+            @TF@N("取得2のタイプが X の場合") fun `fetch2 type is X`(): DynamicNode {
                 every { repository.fetch2() } returns Result2("X")
                 t()
-                return "その他 が XX で表示" { Assertions.assertEquals("XX", actual.elseValue) }
+                return "その他 が XX で表示" { actual.elseValue shouldBe "XX" }
             }
-            @TF fun `取得2のタイプが Y の場合`(): DynamicNode {
+            @TF@N("取得2のタイプが Y の場合") fun `fetch2 type is Y`(): DynamicNode {
                 every { repository.fetch2() } returns Result2("Y")
                 t()
-                return "その他 が YY で表示" { Assertions.assertEquals("YY", actual.elseValue) }
+                return "その他 が YY で表示" { actual.elseValue shouldBe "YY" }
             }
-            @TF fun `取得2のタイプが A でも B でもない場合`(): DynamicNode {
+            @TF@N("取得2のタイプが A でも B でもない場合") fun `fetch2 type is not X, Y`(): DynamicNode {
                 every { repository.fetch2() } returns Result2("Z")
                 t()
-                return "その他 が BB で表示" { Assertions.assertEquals("BB", actual.elseValue) }
+                return "その他 が BB で表示" { actual.elseValue shouldBe "BB" }
             }
         }
-        @TF fun `取得1のタイプが A でも B でもない場合`(): List<DynamicNode> {
+        @TF@N("取得1のタイプが A でも B でもない場合") fun `fetch type is not A, B`(): List<DynamicNode> {
             every { repository.fetch1() } returns Result("C")
             t()
             return listOf(
-                "その他 が CC で表示" { Assertions.assertEquals("CC", actual.elseValue) },
-                "リポジトリの取得2がされない" { verify(exactly = 0) { repository.fetch2() } },
+                "その他 が CC で表示" { actual.elseValue shouldBe "CC" },
+                "リポジトリの取得2がされない" { notExecute { repository.fetch2() } },
             )
         }
     }
