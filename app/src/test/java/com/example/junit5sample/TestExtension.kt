9c8580c2ce9982398ec0beda1f22d2e15c575a84
kiyoshisaito <kiyoshi.saito@tttsunagari.jp>
2024-05-11 20:25:38 +0900

    ファイル名変更と it を 無名に変更

--- app/src/test/java/com/example/junit5sample/TestExtension.kt
+++ app/src/test/java/com/example/junit5sample/TestExtension.kt
@@ -2,16 +2,13 @@ package com.example.junit5sample
 
 import io.mockk.MockKVerificationScope
 import io.mockk.verify
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
 
 
-typealias BE = BeforeEach
 typealias N = DisplayName
 typealias T = Test
 typealias Ne = Nested
